50978341d7a5083ad6a5885905f0c9182fe6a033
Alan Gomez Peralta <alang@MacBook-Pro.local>
2024-04-11 23:04:25 -0400

    show certificates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes:{
+				appear:{
+					'0%': {
+						transform: 'scale(0.8)'
+					},
+					'100%': {
+						transform: 'scale(1)'
+					},
+					
+
+					
+				}
+			},
+			animation:{
+				appear: 'appear 0.7s ease-in-out '
+			}
+		},
 	},
 	plugins: [],
 }
